3941ddcaba17ce1a92354f68a79248cb10b2e711
ftmmohd2000 <ftmmohd2000@gmail.com>
2020-06-07 00:31:03 +0530

    removed Help component from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ function Header() {
       <h1>Expensify</h1>
       <NavLink to="/" activeClassName="is-active" exact={true}>Homepage</NavLink>
       <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
   )
 }
